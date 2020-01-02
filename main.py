5a48a5d46629956f0d729bd37f1b17426fe09258
Shriram <shriram29199@gmail.com>
2020-01-02 10:58:58 +0530

    test

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 from openpyxl import Workbook, load_workbook
 from openpyxl.drawing.image import Image
 from openpyxl.utils.dataframe import dataframe_to_rows
+from datetime import datetime
+
+now=datetime.now()
+filename=now.strftime("./out/%d-%m-%Y_%H-%M-%S.xlsx")
+
+wb = Workbook()
+wb.save(filename)
