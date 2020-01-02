7431445b508999197870c6d6ec43a99d5983d210
Shriram <shriram29199@gmail.com>
2020-01-02 20:36:20 +0530

    :100:

--- main.py
+++ main.py
@@ -2,17 +2,22 @@ from openpyxl import Workbook, load_workbook
 from openpyxl.drawing.image import Image
 from openpyxl.utils.dataframe import dataframe_to_rows
 from datetime import datetime
+import os
 
-inputFilePath="./in/2.txt"
+def clear():
+    if os.name == 'nt':
+        _ = os.system('cls')
+    else:
+        _ = os.system('clear')
 
-now=datetime.now()
-filename=now.strftime("./out/%d-%m-%Y_%H-%M-%S.xlsx")
-
-#
-# wb.save(filename)
-# wb = load_workbook(filename)
-#
-# sheet1 =
+def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    prefix="Line "+str(iteration)+" of "+str(total)
+    print('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix), end = printEnd)
+    if iteration == total:
+        print()
 
 def parse(x):
     out=[]
@@ -23,15 +28,51 @@ def parse(x):
             out.append(itm.replace("_",":"))
     return out
 
+now=datetime.now()
+outFilename=now.strftime("./out/%d-%m-%Y_%H-%M-%S.xlsx")
+loopvar=0
+path = './in/'
+fileNalmes = []
+for r, d, f in os.walk(path):
+    for file in f:
+        if '.txt' in file:
+            fileNalmes.append(file)
+
+clear()
+print("\n=== TXT to XLSX ===\n")
+for file in fileNalmes:
+    print(str(loopvar)+" | "+file)
+    loopvar+=1
+print("\nSelect File: ",end="")
+intimp=int(input())
 try:
+    inputFilePath=path+fileNalmes[intimp]
+    with open(inputFilePath) as inputFile:
+        print("\n=== Deatils ===")
+        print("\nOpened : "+inputFilePath[5:])
+        for lineno, l in enumerate(inputFile):
+            pass
+        lineno=lineno+1
+        print("\n"+str(lineno)+" lines in total\n")
     with open(inputFilePath) as inputFile:
         wb = Workbook()
         wb.create_sheet('sheet1',0)
         active = wb['sheet1']
+        processStart=datetime.now()
         line = inputFile.readline()
+        loopvar=1
+        print("=== Processing ===\n")
+        printProgressBar(loopvar, lineno, length = 50)
         while line:
            active.append(parse(line.split("\t|\t")))
            line = inputFile.readline()
-        wb.save(filename)
+           printProgressBar(loopvar, lineno,  length = 50)
+           loopvar+=1
+        processEnd=datetime.now()
+        print("\n=== Output ===")
+        print("\nProcessing Took : "+str(processEnd-processStart))
+        wb.save(outFilename)
+        print("\nFile Created :"+outFilename)
 except Exception as error:
-    print("Error :" + repr(error)+" ")
+    print("\nError :" + repr(error)+" ")
+print("\n=====================\n")
